4facee69bf3247b009148759fcdc1dfb888fbe08
shadrack-star <shaddkiplangat98@gmail.com>
2024-03-22 23:31:57 +0300

    javscript variables const &let

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+{
+const companyName = "scuber";
+
+}
+
+const mostProfitableNeighborhood = 'Chelsea';{
+let mostProfitableNeighborhood =`Chelsea`;
+}
+const companyCeo=`Susan Smith`;
+{
+let companyCeo="Susan Smith";
+}
